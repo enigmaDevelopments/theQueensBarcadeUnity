c316f217b43fe3ad475270cce893de9f5dcc9b05
enigmaDevelopments <enigmarezac@gmail.com>
2026-01-02 17:50:04 -0500

    change colors

--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -8,6 +8,13 @@ public struct Board
 }
 public class MouseClick : MonoBehaviour
 {
+    public SpriteRenderer boarder;
+    public SpriteRenderer tile;
+    public Color originalBoarderColor;
+    public Color selectedBoarderColor;
+    public Color originalTileColor;
+    public Color blockedTileColor;
+    [Header("Tile Info")]
     public byte index;
 
     public static Transform[] queens;
@@ -17,16 +24,23 @@ public class MouseClick : MonoBehaviour
         p1 = 0xDE,
         p2 = 0x3
     };
+    private static bool initialized = false;
+
     private void Awake()
     {
+        boarder.color = originalBoarderColor;
+        tile.color = originalTileColor;
+        if (initialized) return;
+        initialized = true;
+        Debug.Log(index);
         queens = new Transform[4];
-        for (int i = 1; i <= 4; i++)
-            queens[i] = GameObject.FindGameObjectWithTag("queen" + i).transform;
+        for (int i = 0; i < 4; i++)
+            queens[i] = GameObject.FindGameObjectWithTag("queen" + (i + 1)).transform;
     }
     void OnMouseDown()
     {
         Debug.Log(index);
-        
-
+        boarder.color = selectedBoarderColor;
+        tile.color = blockedTileColor;
     }
 }
