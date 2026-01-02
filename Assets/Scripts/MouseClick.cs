98f407fe8c28eda81269dfb42165ef15ac881a4b
enigmaDevelopments <enigmarezac@gmail.com>
2026-01-02 16:52:05 -0500

    set up bord struct

--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+public struct Board
+{
+    public ushort blocks;
+    public byte p1;
+    public byte p2;
 
+}
 public class MouseClick : MonoBehaviour
 {
-    private void OnMouseDown()
+    public static Board board = new Board
+    {
+        blocks = 0,
+        p1 = 0xDE,
+        p2 = 0x3
+    };
+
+
+    void OnMouseDown()
     {
         Debug.Log("Mouse clicked on " + gameObject.name);
+        
+
     }
 }
+
