0a3418969986872904274bbb8ed4752d16260e8d
enigmaDevelopments <enigmarezac@gmail.com>
2026-05-12 19:29:26 -0400

    find all possible moves

--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -1,11 +1,11 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.InputSystem.LowLevel;
-using UnityEngine.UI;
+
 public class MouseClick : MonoBehaviour
 {
+    
     public SpriteRenderer boarder;
     public SpriteRenderer tile;
     public Color originalBoarderColor;
@@ -156,9 +156,9 @@ public class MouseClick : MonoBehaviour
             ushort blocks = (ushort)board.Data;
             uint queen = (uint)1 << board[queenLocations[i]];
             queen |= (uint)1 << board[queenLocations[i + 1]];
-            uint surrounding = (((uint)0b10_0000_0000_0010_0010 << board[queenLocations[i]]) | ((uint)0b10_0000_0000_0010_0010 << board[queenLocations[i + 1]])) & 0b1_0001_0001_1111_0001_0001_0001_0001;
-            surrounding |= ((queen * 0b1000_0000_0000_1000) | (queen >> 1)) & 0b1000_1000_1000_1111_1000_1000_1000_1000;
-            surrounding |= (queen * 0b1_0000_0000_0001_0000) & 0b1111_0000_0000_0000_0000;
+            uint surrounding = (((uint)0b10_0000_0000_0010_0010 << board[queenLocations[i]]) | ((uint)0b10_0000_0000_0010_0010 << board[queenLocations[i + 1]])) & 0b1110_1110_1110_0000_1110_1110_1110_1110;
+            surrounding |= ((queen * 0b1000_0000_0000_1000) | (queen >> 1)) & 0b111_0111_0111_0000_0111_0111_0111_0111;
+            surrounding |= (queen * 0b1_0000_0000_0001_0000) & 0b1111_1111_1111_0000_1111_1111_1111_1111;
             ushort smallSurounding = (ushort)(surrounding | (surrounding >> 20));
             blocks = (ushort)~(blocks | queen);
             smallSurounding &= blocks;
@@ -176,7 +176,7 @@ public class MouseClick : MonoBehaviour
             queenPosition = (byte)board[queenLocations[0]];
         int lower = Mathf.Min(queen, position);
         int upper = Mathf.Max(queen, position);
-        foreach (int direction in new int[] {5, 4, 3, 1})
+        foreach (int direction in directions)
             if ((upper-lower) % direction == 0)
             {
                 bool vaild = true;
@@ -193,5 +193,82 @@ public class MouseClick : MonoBehaviour
                     return true;
             }
         return false;
+    }
+
+    private static readonly byte[] directions = { 1, 3, 4, 5 };
+    private static readonly uint[] walls = {0b1110_1110_1110_1110, 0b111_0111_0111_0111, uint.MaxValue, 0b1110_1110_1110_1110, 0b111_0111_0111_0111,0b1110_1110_1110_1110,uint.MaxValue,0b111_0111_0111_0111};
+
+    public Queue<BitVector32> getMoves(BitVector32 board, bool p1Turn)
+    {
+        Queue<BitVector32> output = new Queue<BitVector32>(38);
+        uint[] pieces;
+        if (p1Turn)
+            pieces = new uint[] {
+                (uint)(1 << board[queenLocations[0]]),
+                (uint)(1 << board[queenLocations[1]]),
+                (uint)((1 << board[queenLocations[2]]) | (1 << board[queenLocations[3]]))
+            };
+        else
+            pieces = new uint[] {
+                (uint)(1 << board[queenLocations[0]]),
+                (uint)(1 << board[queenLocations[1]]),
+                (uint)((1 << board[queenLocations[2]]) | (1 << board[queenLocations[3]]))
+            };
+        uint semiBlock = (ushort)((uint)board.Data | pieces[0] | pieces[1]);
+        uint block = semiBlock | pieces[2];
+        for (uint i = 1; i < 0b1000000000000000; i<<=1)
+            if ((block & i) == 0)
+                output.Enqueue(new BitVector32(board.Data | (int)i));
+        semiBlock = ~semiBlock;
+        block = ~block;
+        uint[] pieceMoves = {0, 0};
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                uint piece = pieces[i];
+                for (int k = 0; k < 3; k++)
+                {
+                    piece <<= directions[j];
+                    piece &= walls[j];
+                    pieceMoves[i] |= piece & semiBlock;
+                    piece &= block;
+                    if (piece == 0)
+                        break;
+                }
+
+            }
+            if (pieces[0] == pieces[1])
+                break;
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                uint piece = pieces[i];
+                for (int k = 0; k < 3; k++)
+                {
+                    piece >>= directions[j];
+                    piece &= walls[j + 4];
+                    pieceMoves[i] |= piece & semiBlock;
+                    piece &= block;
+                    if (piece == 0)
+                        break;
+                }
+            }
+            if (pieces[0] == pieces[1])
+                break;
+        }
+        for (uint i = 1, j = 0; j < 16; i <<= 1, j++)
+            for (int k = 0; k < 2; k++)
+                if ((pieceMoves[k] & i) == 0)
+                {
+                    BitVector32 newBoard = new BitVector32(board.Data);
+                    newBoard[queenLocations[(p1Turn ? 0 : 2) + k]] = (int)j;
+                    output.Enqueue(newBoard);
+                }
+        output.TrimExcess();
+        return output;
     }
 }
